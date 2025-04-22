89feb54eb7df70bcba07e8a867772dc4197a1e53
Frida Jansson <frja2400@student.miun.se>
2025-04-22 12:34:07 +0200

    Skapar en asynkron funktion med fetch-anrop som hämtar data från min databas. Skapar en andra funktion som manipulerar DOM och skriver ut min data till skärmen.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,44 @@
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Boilerplate är laddad och klar!');
-});
+//Asynkron funktion med try/catch för att hämta ut min data. 
+async function getData() {
+
+    try {
+
+        const response = await fetch("https://dt207g-moment2-rest.onrender.com/api/workexperience");
+
+        if (!response.ok) throw new Error("Nätverksfel");
+
+        const data = await response.json();
+        console.table(data);
+        renderData(data);   //Anropar funktion som skriver ut datan på skärmen.
+    } catch (error) {
+        console.error("Fel vid hämtning:", error);
+    } finally {
+        console.log("Förfrågan avslutad.");
+    }
+}
+
+getData();
+
+//Skriv ut datan på skärmen med en funktion och manipulera DOM
+function renderData(data) {
+    const tableBody = document.getElementById("workexperienceTable");
+    tableBody.innerHTML = '';
+
+    data.forEach(work => {
+        const row = document.createElement('tr');
+
+        //Addera varje egenskap från de egenskaperna som finns i mitt objekt i API:et.
+        row.innerHTML = `
+            <td>${work.companyname}</td>
+            <td>${work.jobtitle}</td>
+            <td>${work.location}</td>
+            <td>${work.startdate}</td>
+            <td>${work.enddate}</td>
+            <td>${work.description}</td>
+        `;
+
+        tableBody.appendChild(row);
+    });
+}
